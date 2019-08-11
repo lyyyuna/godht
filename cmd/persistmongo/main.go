2071b91a5c07a009ac13f247d23bf3db011295b5
lyyyuna <lyyyuna@gmail.com>
2019-08-11 20:24:26 +0800

    update

--- cmd/persistmongo/main.go
+++ cmd/persistmongo/main.go
@@ -14,13 +14,14 @@ func main() {
 	port := flag.String("port", "6882", "Listening port")
 	limit := flag.Int("limit", 500, "Friends made upper limit per second, default 500/seconds")
 	rejoin := flag.Int("rejoin", 60, "Rejoin the DHT bootstrap rate, default 60 seconds")
+	mongoserver := flag.String("ms", "127.0.0.1:27017", "Specfify the mongo server address, default 127.0.0.1:27017")
 
 	flag.Parse()
 	addr := fmt.Sprintf("0.0.0.0:%s", *port)
 
 	defer glog.Flush()
 	// Init mongo client, persistent store
-	mp, err := mongointegr.NewMongoClient("127.0.0.1:27017")
+	mp, err := mongointegr.NewMongoClient(*mongoserver)
 	if err != nil {
 		fmt.Println(err)
 		glog.Error(err)
